61198706a879e7ba0af7fccaa7779abf4564d132
杨兵 <fainle@qq.com>
2019-03-18 21:55:31 +0800

    add jquery.min.js file

--- webapp/route/admin.py
+++ webapp/route/admin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 admin_bp = Blueprint('admin_bp', __name__)
 
@@ -10,6 +10,13 @@ def admin_index():
     return 'admin index'
 
 
-@admin_bp.route('/admin/login')
+@admin_bp.route('/admin/login', methods=['get', 'post'])
 def admin_login():
+    if request.method == 'post':
+        username = request.form['username']
+        password = request.form['password']
+
+        # todo check pass
+        pass
+
     return render_template('admin/login.html')
