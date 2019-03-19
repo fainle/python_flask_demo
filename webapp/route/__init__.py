1db1beb34eaa09e648fca0c9b21fabc6ac91b632
杨兵 <fainle@qq.com>
2019-03-19 08:52:48 +0800

    base iframe page

--- webapp/route/__init__.py
+++ webapp/route/__init__.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import functools
+from flask import url_for, redirect, session
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def _(**kwargs):
+        if session.get('admin_name') is None:
+            return redirect(url_for('admin_bp.admin_login'))
+
+        return view(**kwargs)
+    return _
--- webapp/route/admin.py
+++ webapp/route/admin.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session, redirect, url_for
 
 admin_bp = Blueprint('admin_bp', __name__)
 
 
 @admin_bp.route('/')
 def admin_index():
-    return 'admin index'
+
+    next = request.args.get('next', 'welcome')
+    return render_template('layout/base_iframe.html', next=next)
 
 
 @admin_bp.route('/admin/login', methods=['get', 'post'])
@@ -15,8 +17,23 @@ def admin_login():
     if request.method == 'post':
         username = request.form['username']
         password = request.form['password']
+        error = None
 
-        # todo check pass
-        pass
+        data = dict()
+
+        if error is None:
+            session.clear()
+            session['admin_name'] = username
+            data['status_code'] = 1
+            data['url'] = url_for('admin_bp.admin_index')
+        else:
+            data['status_code'] = 0
+            data['msg'] = error
 
     return render_template('admin/login.html')
+
+
+@admin_bp.route('/admin/logout')
+def admin_logout():
+    session.clear()
+    return redirect(url_for('admin_bp.admin_index'))
